bcf4402f2014aac420e43d22488a178873d649b0
손소희 <ssh30510044@gmail.com>
2022-08-10 14:13:38 +0900

    Add: home 페이지의 header 부분 ui 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import HomeHeader from '../../components/Home/HomeHeader';
 
 function Home() {
-  return <>타입스크립트</>;
+  return (
+    <Container>
+      <HomeHeader />
+      <ViewContent></ViewContent>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  padding: 110px 0 0 0;
+`;
+
+const ViewContent = styled.div`
+  width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 40px 0;
+`;
 
 export default Home;
